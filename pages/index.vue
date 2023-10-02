f06a3f49675b6c1d11624d33870ec99136037416
Aaloke <cuba2419@gmail.com>
2023-10-02 14:20:25 +0530

    Added Icons and Improved Code.
    Co-authored-by: Hemanth Tenneti <hemanth10etii@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,36 @@ const istFormattedDateTime = istDateTime.toLocaleString("en-IN", options);
 
 <template>
   <main>
-    <div class="flex justify-center items-center w-screen h-screen">
+    <div
+      class="front-content absolute text-white flex flex-col justify-center items-center h-screen w-screen z-10"
+    >
+      <h1 class="font-bold text-[130px]">Hemanth Tenneti</h1>
+      <p class="text-center text-[35px] mt-[-40px] mb-5">
+        A passion for programming with an insatiable curiosity<br />
+        fuelling my portfolio
+      </p>
+      <div class="flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="#fff"
+          viewBox="0 0 256 256"
+        >
+          <path
+            d="M128,24a80,80,0,0,0-80,80c0,72,80,128,80,128s80-56,80-128A80,80,0,0,0,128,24Zm0,112a32,32,0,1,1,32-32A32,32,0,0,1,128,136Z"
+            opacity="0.2"
+          ></path>
+          <path
+            d="M128,64a40,40,0,1,0,40,40A40,40,0,0,0,128,64Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,128Zm0-112a88.1,88.1,0,0,0-88,88c0,31.4,14.51,64.68,42,96.25a254.19,254.19,0,0,0,41.45,38.3,8,8,0,0,0,9.18,0A254.19,254.19,0,0,0,174,200.25c27.45-31.57,42-64.85,42-96.25A88.1,88.1,0,0,0,128,16Zm0,206c-16.53-13-72-60.75-72-118a72,72,0,0,1,144,0C200,161.23,144.53,209,128,222Z"
+          ></path>
+        </svg>
+        <p class="text-[25px] text-[#d7d7d7]">Hyderabad, India</p>
+      </div>
+    </div>
+    <div
+      class="absolute m-auto w-screen h-screen flex justify-center items-center"
+    >
       <h1
         class="bg-gradient-to-b from-[#2F2F2F] to-[#101012] bg-clip-text text-transparent font-bold text-[150px] lg:text-[600px]"
       >
