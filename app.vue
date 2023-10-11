ac77147806081502bafa6b6e3b8de6ca91abb8e5
Aaloke <cuba2419@gmail.com>
2023-10-11 22:48:37 +0530

    Switched to SvelteKit, Major UI color changes
    Co-authored-by: Hemanth Tenneti <hemanth10etii@gmail.com>

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bg-[#0D0D0E] h-screen w-screen">
-    <div class="bg-[url('/background.png')] h-screen w-screen flex">
-      <Navbar />
-      <NuxtPage />
-      <Footer />
-    </div>
-  </div>
-</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  darkMode: "class",
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,css,js,svelte,ts}'],
+	darkMode: 'class',
+	theme: {
+		extend: {}
+	},
+	plugins: []
 };
